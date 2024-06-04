55c56e1e598bb8fad89906ac109fce1c344c4b3b
Fellyipe <felipebueno2004@hotmail.com>
2024-06-04 20:41:24 -0300

    Criação do DataBase "academia.db"

--- Sistema de academia/ProjetoAcademia/obj/Debug/net8.0/ProjetoAcademia.AssemblyInfo.cs
+++ Sistema de academia/ProjetoAcademia/obj/Debug/net8.0/ProjetoAcademia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAcademia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de292bef05effa933c6b7ecd7d277efa0b24fe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5cae4e72674029c378d5e50151205fbbaf773e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAcademia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAcademia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
