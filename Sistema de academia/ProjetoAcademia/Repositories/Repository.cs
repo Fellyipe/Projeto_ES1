dfeede1802b5147cdd5bc48635e83d2243f509c3
Fellyipe <felipebueno2004@hotmail.com>
2024-06-04 21:39:05 -0300

    Alteração da classe Repository para uso do EF Core

--- Sistema de academia/ProjetoAcademia/Repositories/Repository.cs
+++ Sistema de academia/ProjetoAcademia/Repositories/Repository.cs
@@ -1,40 +1,43 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace ProjetoAcademia
+public class Repository
 {
-    public class Repository
+    private readonly DbContext dbContext;
+
+    public Repository(DbContext dbContext)
     {
-        private readonly string connectionString;
+        this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
 
-        public Repository(string connectionString)
-        {
-            this.connectionString = connectionString;
-        }
+    public IEnumerable<T> GetAll<T>() where T : class
+    {
+        return dbContext.Set<T>().ToList();
+    }
 
-        public void Create<T>(T entity) where T : class
-        {
-            // Implementação do método Create
-        }
+    public T GetById<T>(int id) where T : class
+    {
+        return dbContext.Set<T>().Find(id);
+    }
 
-        public T Read<T>(int id) where T : class
-        {
-            // Implementação do método Read
-        }
+    public void Create<T>(T entity) where T : class
+    {
+        dbContext.Set<T>().Add(entity);
+        dbContext.SaveChanges();
+    }
 
-        public void Update<T>(T entity) where T : class
-        {
-            // Implementação do método Update
-        }
+    public void Update<T>(T entity) where T : class
+    {
+        dbContext.Entry(entity).State = EntityState.Modified;
+        dbContext.SaveChanges();
+    }
 
-        public void Delete<T>(int id) where T : class
-        {
-            // Implementação do método Delete
-        }
-
-        public IEnumerable<T> ListAll<T>() where T : class
-        {
-            // Implementação do método ListAll
-        }
+    public void Delete<T>(int id) where T : class
+    {
+        var entity = dbContext.Set<T>().Find(id);
+        dbContext.Set<T>().Remove(entity);
+        dbContext.SaveChanges();
     }
 }
